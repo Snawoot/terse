73ac2620d09b339c966a7ae640108137f1e134a0
Vladislav Yarmak <vladislav@vm-0.com>
2023-02-02 16:53:34 +0200

    CLI finished

--- cmd/terse/main.go
+++ cmd/terse/main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"bufio"
+	"bytes"
 	"flag"
 	"fmt"
 	"os"
@@ -13,8 +15,9 @@ const (
 var (
 	version = "undefined"
 
-	showVersion = flag.Bool("version", false, "show program version and exit")
-	limit  = flag.Int("n", 25, "number of lines to sample")
+	showVersion  = flag.Bool("version", false, "show program version and exit")
+	limit        = flag.Int("n", 25, "number of lines to sample")
+	nulDelimiter = flag.Bool("z", false, "line delimiter is NUL, not newline")
 )
 
 func usage() {
@@ -36,9 +39,41 @@ func run() int {
 		return 0
 	}
 
+	scanner := bufio.NewScanner(os.Stdin)
+	if *nulDelimiter {
+		scanner.Split(scanZeroTerminatedLines)
+	}
+	for scanner.Scan() {
+		fmt.Printf("read: %q\n", scanner.Text())
+	}
+
+	if err := scanner.Err(); err != nil {
+		fmt.Fprintf(os.Stderr, "read error: %v", err)
+	}
+
 	return 0
 }
 
 func main() {
 	os.Exit(run())
+}
+
+// scanZeroTerminatedLines is a split function for a Scanner that returns each line of
+// text, stripped of any trailing zero-byte end-of-line marker.
+// The returned line may be empty.
+// The last non-empty line of input will be returned even if it has no newline.
+func scanZeroTerminatedLines(data []byte, atEOF bool) (advance int, token []byte, err error) {
+	if atEOF && len(data) == 0 {
+		return 0, nil, nil
+	}
+	if i := bytes.IndexByte(data, 0); i >= 0 {
+		// We have a full zero-terminated line.
+		return i + 1, data[0:i], nil
+	}
+	// If we're at EOF, we have a final, non-terminated line. Return it.
+	if atEOF {
+		return len(data), data, nil
+	}
+	// Request more data.
+	return 0, nil, nil
 }
